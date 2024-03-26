01ddbe849857bcdfad591df88308938bb4ca952d
Michel Moncada <moncadarabassam@berea.edu>
2024-03-26 15:49:27 +0000

    Created the tables in the models file using pewee and created a file called app.py which is where our app will run

---
+++ flaskapp/app.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, request
+
+from models import *
+#from logic import *
+
+#to run type flask run in the terminal
+app = Flask(__name__)
--- flaskapp/models.py
+++ flaskapp/models.py
@@ -10,13 +10,66 @@ class baseModel(Model):
 
 # Class Definitions
 # https://docs.peewee-orm.com/en/latest/peewee/models.html
-class OtherTable (baseModel):
-    otherid = PrimaryKeyField()
-    data = CharField(null=False)
+#class OtherTable (baseModel):
+ #   otherid = PrimaryKeyField()
+  #  data = CharField(null=False)
 
-class Example (baseModel):
-    username = CharField(32,unique=True)
-    description = CharField(255)
-    other = ForeignKeyField(OtherTable)
-    isInt = BooleanField(default=0)
-    isBool = BooleanField()
+#class Example (baseModel):
+#    username = CharField(32,unique=True)
+#    description = CharField(255)
+#    other = ForeignKeyField(OtherTable)
+#    isInt = BooleanField(default=0)
+#    isBool = BooleanField()
+
+class Volunteer(baseModel):
+    VolunteerID = IntegerField(primary_key=True)
+    FirstName = CharField(max_length=255)
+    LastName = CharField(max_length=255)
+    Email = CharField(max_length=255)
+    Phone = CharField(max_length=20)
+    HasRecordAccess = BooleanField()
+    
+class Neighbor(baseModel):
+    NeighborID = IntegerField(primary_key=True)
+    VolunteerID = ForeignKeyField(Volunteer, backref='neighbors')
+    Organization = CharField(max_length=255)
+    FirstName = CharField(max_length=255)
+    LastName = CharField(max_length=255)
+    DateOfBirth = DateField()
+    Phone = CharField(max_length=20)
+    Address = TextField()
+    Email = CharField(max_length=255)
+    HasStateID = BooleanField()
+    HasPet = BooleanField()
+
+class Service_Providers(baseModel):
+    Organization = CharField(max_length=255, primary_key=True)
+    ContactPerson = CharField(max_length=255)
+    Email = CharField(max_length=255)
+    Phone = CharField(max_length=20)
+    DateOfStart = DateField()
+
+class Services(baseModel):
+    ServiceType = CharField(max_length=255, primary_key=True)
+    Organization = ForeignKeyField(Service_Providers, backref='services')
+
+class Inventory(baseModel):
+    NameOfItem = CharField(max_length=255, primary_key=True)
+    VolunteerID = ForeignKeyField(Volunteer, backref='inventory')
+    ExpirationDate = DateField()
+    NumberOfItem = IntegerField()
+
+class Visit_Service(baseModel):
+    ServiceOrder = IntegerField(primary_key=True)
+    Organization = ForeignKeyField(Service_Providers, backref='visit_services')
+    NeighborID = ForeignKeyField(Neighbor, backref='visit_services')
+    ServiceType = ForeignKeyField(Services, backref='visit_services')
+    Description = TextField()
+    VolunteerID = ForeignKeyField(Volunteer, backref='visit_services')
+
+class Visit_Record(baseModel):
+    RecordID = IntegerField(primary_key=True)
+    ServiceOrder = ForeignKeyField(Visit_Service, backref='visit_records')
+    Date = DateField()
+    Time = DateField()
+    Notes = TextField()
