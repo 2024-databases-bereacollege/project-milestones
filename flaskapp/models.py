cf9cb044cbeb453be4d8c592242c8e9286b0b0d8
M-Ali-Ramazani <112561860+ramazanima@users.noreply.github.com>
2024-03-29 20:57:43 +0000

    completed milestone four

--- flaskapp/models.py
+++ flaskapp/models.py
@@ -10,13 +10,55 @@ class baseModel(Model):
 
 # Class Definitions
 # https://docs.peewee-orm.com/en/latest/peewee/models.html
-class OtherTable (baseModel):
-    otherid = PrimaryKeyField()
-    data = CharField(null=False)
+# class OtherTable (baseModel):
+#     otherid = PrimaryKeyField()
+#     data = CharField(null=False)
 
-class Example (baseModel):
-    username = CharField(32,unique=True)
-    description = CharField(255)
-    other = ForeignKeyField(OtherTable)
-    isInt = BooleanField(default=0)
-    isBool = BooleanField()
+# class Example (baseModel):
+#     username = CharField(32,unique=True)
+#     description = CharField(255)
+#     other = ForeignKeyField(OtherTable)
+#     isInt = BooleanField(default=0)
+#     isBool = BooleanField()
+
+class member (baseModel):
+    memberId = PrimaryKeyField()
+    firstName = CharField(32,unique=True)
+    lastName = CharField(32,unique=True)
+    phoneNumber = CharField(20,unique=True)
+    score = IntegerField()
+    NumberOfEventsAttended = IntegerField()
+
+class chapter(baseModel):
+    chapterName = PrimaryKeyField()
+    numberofMembers = IntegerField()
+    chapterLead = CharField(32,unique=True)
+    memberId = ForeignKeyField(member)
+    chapterEmail = CharField(32,unique=True)
+
+class event:
+    eventName = PrimaryKeyField()
+    venue = CharField(32,unique=True)
+    eventDate = DateTimeField(default=datetime.datetime.utcnow)
+    attendance = IntegerField()
+
+class demographics:
+    memberId = ForeignKeyField(member),
+    race = CharField(32,unique=True)
+    age = IntegerField()
+    gender = CharField(32,unique=True)
+
+class memberAddress:
+    memberId = ForeignKeyField(member),
+    street = CharField(32,unique=True)
+    city = CharField(32,unique=True)
+    state =CharField(32,unique=True) 
+    zipcode = IntegerField()
+
+class donation: 
+    memberId = ForeignKeyField(member),
+    item = CharField(32,unique=True)
+    monetaryWorth = IntegerField()
+
+
+
