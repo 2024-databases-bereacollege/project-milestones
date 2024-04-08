844ccd0a9298eb249fffeed76f326a260a5da7fe
Michel Moncada <moncadarabassam@berea.edu>
2024-04-08 13:37:49 +0000

    I finished the neighbor table and I finished the Service Provider table, they are ready for their correspondings html

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -51,38 +51,99 @@ def login():
 @app.route('/logout')
 def logout():
     session.clear()
-    return redirect(url_for('index.html'))
+    return redirect(url_for('login.html'))
 
 # Route for adding a neighboor
 @app.route('/add', methods=['POST'])
-def add_Neighbor():
-    if request.method == ['POST']:
-        FirstName = request.form['FirstName']
-        LastName = request.form['LastName']
-        Organization = request.form['Organization']
-        Email = request.form['Email']
-        DateOfBirth = request.form['DateofBirth']
-        Phone = request.form['Phone']
-        Address = request.form['Address']
+def add_neighbor():
+    if request.method == 'POST':
+        neighbor_id = request.form.get('NeighborID')
+        edit = request.args.get('edit') == 'true'
         
-        HasPet = request.form.get('HasPet')
-        if HasPet.lower() not in ['yes', 'no']:
-            return render_template('form.html', error='Please enter "True" or "False" for the boolean input')
+        if neighbor_id and edit:
+            neighbor = Neighbor.query.get(neighbor_id)
+            if neighbor:
+                neighbor.FirstName = request.form['FirstName']
+                neighbor.LastName = request.form['LastName']
+                neighbor.Organization = request.form['Organization']
+                neighbor.Email = request.form['Email']
+                neighbor.DateOfBirth = datetime.datetime.strptime(request.form['DateOfBirth'], '%Y-%m-%d')
+                neighbor.Phone = request.form['Phone']
+                neighbor.Address = request.form['Address']
+                neighbor.HasPet = request.form.get('HasPet')
+                neighbor.HasStateID = request.form.get('HasStateID')
+                mydb.session.commit()
+                return redirect(url_for('neighbor_list'))
+            else:
+                return render_template('error.html', message='Neighbor not found')
+        else:
+            new_neighbor = Neighbor(
+                FirstName=request.form['FirstName'],
+                LastName=request.form['LastName'],
+                Organization=request.form['Organization'],
+                Email=request.form['Email'],
+                DateOfBirth=datetime.datetime.strptime(request.form['DateOfBirth'], '%Y-%m-%d'),
+                Phone=request.form['Phone'],
+                Address=request.form['Address'],
+                HasPet=request.form.get('HasPet'),
+                HasStateID=request.form.get('HasStateID')
+            )
+            mydb.session.add(new_neighbor)
+            mydb.session.commit()
+            return redirect(url_for('neighbor_list'))
+    else:
+        neighbor_id = request.args.get('NeighborID')
+        edit = request.args.get('edit') == 'true'
         
-        HasStateID = request.form.get('StateID')
-        if HasStateID.lower() not in ['yes', 'no']:
-            return render_template('form.html', error='Please enter "True" or "False" for the boolean input')
-
-        try:
-            Neighbor.create(FirstName = FirstName, LastName = LastName, Organization = Organization, Email = Email,
-                            DateOfBirth = DateOfBirth, Phone = Phone, Address = Address,
-                            HasPet = HasPet, HasStateID = HasStateID)
-            return redirect(url_for('add_neighbor.html'))
-        except IntegrityError:
-            # Handle duplicate username or email
-            return render_template('add_neighbor.html', error='Neighbor already exists')
-    return "Form submitted successfully!"
-
+        if neighbor_id and edit:
+            neighbor = Neighbor.query.get(neighbor_id)
+            if neighbor:
+                return render_template('edit_neighbor.html', neighbor=neighbor)
+            else:
+                return render_template('error.html', message='Neighbor not found')
+        return render_template('create_neighbor.html')
+    
+@app.route('/add', methods=['POST'])
+def Provider_Services():
+    if request.method == 'POST':
+        organization_id = request.form.get('Organization')
+        edit = request.args.get('edit') == 'true'
+        
+        if organization_id and edit:
+            organization = Service_Providers.query.get(organization_id)
+            if organization:
+                organization.ContactPerson = request.form['ContactPerson']
+                organization.Email = request.form['Email']                
+                organization.Phone = request.form['Phone']
+                organization.DateOfStart = datetime.datetime.strptime(request.form['DateOfStart'], '%Y-%m-%d')
+                mydb.session.commit()
+                return redirect(url_for('service_providers'))  # Redirect to service_providers or another page
+            else:
+                return render_template('error.html', message='Service Provider not found')
+        else:
+            new_service = Service_Providers(
+                Organization=request.form['Organization'],
+                ContactPerson=request.form['ContactPerson'],
+                Email=request.form['Email'],
+                Phone=request.form['Phone'],
+                DateOfStart=datetime.datetime.strptime(request.form['DateOfStart'], '%Y-%m-%d')
+            )
+            mydb.session.add(new_service)
+            mydb.session.commit()
+            return render_template('home.htm', message='Service Provider created')
+        
+    else:
+        organization_id = request.args.get('organization_name')
+        edit = request.args.get('edit') == 'true'
+        
+        if organization_id and edit:
+            organization = Service_Providers.query.get(organization_id)
+            if organization:
+                return render_template('edit_service_provider.html', organization=organization)
+            else:
+                return render_template('home.html', message='Service Provider not found')
+        return render_template('Service_Provider.html')
+    
 ## additions Nicholas
 # Service Providers Example ########################################
 @app.route('/service-providers')
