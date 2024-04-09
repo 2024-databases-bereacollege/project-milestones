6b5516b0f8e51f0eea34d0d6ea91e5d8b41ce0c0
Mostaphe Mohamud <97688286+mohamudm1@users.noreply.github.com>
2024-04-09 18:00:49 +0000

    fixed the problems with the database and jpyter notebook

--- flaskapp/tables.sql
+++ flaskapp/tables.sql
@@ -31,32 +31,3 @@ CREATE TABLE donation (
     monetaryWorth INTEGER
 );
 
-INSERT INTO member (Score, phoneNumber, firstName, middleName, lastName, memberAddress, NumberOfEventsAttended)
-VALUES
-    (987, 1234567890, 'Betty', 'M', 'Hibler', '314 Prospect St, Berea, KY, 40403',  5),
-    (654, 2345678901, 'Landra', '', 'Lewis', '619 Chestnut St, Berea, KY, 40403', 3),
-    (321, 3456789012, 'Travis', '', 'Bolinger', '314 University Dr Apt A, Richmond, KY, 2841',  2),
-    (432, 4567890123, 'Rachel', '',  'White', '1101 Elm St, Danville, KY, 40422', 7),
-    (876, 5678901234, 'Joanie', '', 'Lukins', '503 Ohara Dr, Daville, KY, 40422', 4);
-
-INSERT INTO chapter (chapterName, numberofMembers, chapterLead, chapterEmail)
-VALUES
-    ('Madison County', 50, 'Landra Lewis', 'alex@example.com'),
-    ('Wilderness Trace', 30, 'Emily Brown', 'emily@example.com');
-
-
-INSERT INTO donation (item, monetaryWorth)
-VALUES
-    ('Laptop', 800),
-    ('Books', 100),
-    ('Laptop', 800),
-    ('Books', 100),
-    ('Laptop', 800);
-
-INSERT INTO event (eventName, venue, theme, eventDate, numberofMemebersAttended)
-VALUES
-    ('Community Building', 'Convention Center', 'Environmental Preservation', '2024-04-01', 200),
-    ('Workshop', 'Community Center', 'Environmental Preservation', '2024-04-15', 80),
-    ('Hackathon', 'Convention Center', 'Environmental Preservation', '2024-04-01', 200),
-    ('Seedy Workshop', 'Community Center', 'Environmental Preservation', '2024-04-15', 80),
-    ('How to sleep in class', 'Community Center', 'Sleep', '2024-04-15', 80);
