54a7824328af8d062b701fc568e397440edcb1c5
hamiltonnBC <hamiltonn@berea.edu>
2024-04-15 20:36:12 -0400

    added small changes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,18 +1,18 @@
 // router/index.js
-//import Vue from "vue"; seems to be for vue2
-//import VueRouter from "vue-router"; seems to be for vue2
-import { createRouter, createWebHistory } from 'vue-router' //TODO not sure if needed
-import CRUDTable from "@/components/CRUDTable.vue";
+
+import { createRouter, createWebHistory } from 'vue-router' 
 
 // Import components
+import CRUDTable from "@/components/CRUDTable.vue";
 //import HomePage from '@/components/HomePage.vue'
 //import LoginPage from '@/components/LoginPage.vue'
 // Import other components for routes
 
 const routes = [
+    { path: '/', name : 'home', component: CRUDTable },
  // { path: '/', component: HomePage },
 //  { path: '/login', component: LoginPage },
-    { path: '/CRUDTable', name : 'crudtable', component: CRUDTable },
+  //  { path: '/crudtable', name : 'crudtable', component: CRUDTable },
 
   // Define other routes here
 ]
