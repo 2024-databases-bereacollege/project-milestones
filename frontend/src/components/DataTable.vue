771515b523a015f4903f9e924feeb7d1296c88be
hamiltonnBC <hamiltonn@berea.edu>
2024-04-27 19:11:37 -0400

    updated pages, connecting volunteer to child component now

--- frontend/src/components/DataTable.vue
+++ frontend/src/components/DataTable.vue
@@ -4,88 +4,58 @@
     :items="items"
     :sort-by="[{ key: sortBy, order: sortOrder }]"
   >
-      <template v-slot:top>
-        <v-toolbar
-          flat
-        >
-          <v-toolbar-title>Template table</v-toolbar-title>
-          <v-divider
-            class="mx-4"
-            inset
-            vertical
-          ></v-divider>
-          <v-spacer></v-spacer>
-          <v-dialog
-            v-model="dialog"
-            max-width="500px"
-          >
-            <template v-slot:activator="{ props }">
-              <v-btn
-                class="mb-2"
-                color="primary"
-                dark
-                v-bind="props"
-              >
-                New Item
-              </v-btn>
-            </template>
-            <v-card>
-              <v-card-title>
-                <span class="text-h5">{{ formTitle }}</span>
-              </v-card-title>
-  
-              <v-card-text>
-                <v-container>
-                  <v-row>
-                    <v-col
-                      cols="12"
-                      md="4"
-                      sm="6"
-                    >
-                      <v-text-field
-                        v-model="editedItem.name"
-                        label="Dessert name"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col
-                      cols="12"
-                      md="4"
-                      sm="6"
-                    >
-                      <v-text-field
-                        v-model="editedItem.calories"
-                        label="Calories"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col
-                      cols="12"
-                      md="4"
-                      sm="6"
-                    >
-                      <v-text-field
-                        v-model="editedItem.fat"
-                        label="Fat (g)"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col
-                      cols="12"
-                      md="4"
-                      sm="6"
-                    >
-                      <v-text-field
-                        v-model="editedItem.carbs"
-                        label="Carbs (g)"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col
-                      cols="12"
-                      md="4"
-                      sm="6"
-                    >
-                      <v-text-field
-                        v-model="editedItem.protein"
-                        label="Protein (g)"
-                      ></v-text-field>
+    <template v-slot:top>
+      <v-toolbar flat>
+        <v-toolbar-title>Template table</v-toolbar-title>
+        <v-divider class="mx-4" inset vertical></v-divider>
+        <v-spacer></v-spacer>
+        <v-dialog v-model="dialog" max-width="500px">
+          <template v-slot:activator="{ props }">
+            <v-btn class="mb-2" color="primary" dark v-bind="props">
+              New Item
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">{{ formTitle }}</span>
+            </v-card-title>
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" md="4" sm="6">
+                    <v-text-field
+                      :value="editedItem.name"
+                      @input="$emit('update:editedItem', { ...editedItem, name: $event })"
+                      label="Dessert name"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="4" sm="6">
+                    <v-text-field
+                      :value="editedItem.calories"
+                      @input="$emit('update:editedItem', { ...editedItem, calories: $event })"
+                      label="Calories"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="4" sm="6">
+                    <v-text-field
+                      :value="editedItem.fat"
+                      @input="$emit('update:editedItem', { ...editedItem, fat: $event })"
+                      label="Fat (g)"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="4" sm="6">
+                    <v-text-field
+                      :value="editedItem.carbs"
+                      @input="$emit('update:editedItem', { ...editedItem, carbs: $event })"
+                      label="Carbs (g)"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" md="4" sm="6">
+                    <v-text-field
+                      :value="editedItem.protein"
+                      @input="$emit('update:editedItem', { ...editedItem, protein: $event })"
+                      label="Protein (g)"
+                    ></v-text-field>
                     </v-col>
                   </v-row>
                 </v-container>
@@ -150,7 +120,6 @@
     </v-data-table>
   </template>
 
-  
 <script>
 export default {
   props: {
@@ -160,6 +129,132 @@ export default {
     },
     items: {
       type: Array,
+      required: true,
+    },
+    editedItem: {
+      type: Object,
+      required: true,
+    },
+    defaultItem: {
+      type: Object,
+      required: true,
+    },
+    onSave: {
+      type: Function,
+      required: true,
+    },
+    onDelete: {
+      type: Function,
+      required: true,
+    },
+    onClose: {
+      type: Function,
+      required: true,
+    },
+    onCloseDelete: {
+      type: Function,
+      required: true,
+    },
+    sortBy: {
+      type: String,
+      default: '',
+    },
+    sortOrder: {
+      type: String,
+      default: 'asc',
+    },
+  },
+  data() {
+    return {
+      desserts: [],
+      dialog: false,
+      dialogDelete: false,
+      editedIndex: -1,
+    };
+  },
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? 'New Item' : 'Edit Item';
+    },
+  },
+  watch: {
+    dialog(val) {
+      val || this.close();
+    },
+    dialogDelete(val) {
+      val || this.closeDelete();
+    },
+  },
+  created() {
+    this.initialize();
+  },
+  methods: {
+    initialize() {
+      this.desserts = [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+        },
+      ];
+    },
+    editItem(item) {
+      this.$emit('edit-item', item);
+    },
+    deleteItem(item) {
+      this.$emit('delete-item', item);
+    },
+    deleteItemConfirm() {
+      this.$emit('delete-item-confirm');
+    },
+    close() {
+      this.$emit('close');
+    },
+    closeDelete() {
+      this.$emit('close-delete');
+    },
+    save() {
+      this.$emit('save', this.editedItem);
+    },
+  },
+};
+</script>
+
+<!-- <script>
+export default {
+  props: {
+    headers: {
+      type: Array,
+      required: true,
+    },
+    items: {
+      type: Array,
+      required: true,
+    },
+    editedItem: {
+      type: Object,
+      required: true,
+    },
+    defaultItem: {
+      type: Object,
+      required: true,
+    },
+    onSave: {
+      type: Function,
+      required: true,
+    },
+    onDelete: {
+      type: Function,
+      required: true,
+    },
+    onClose: {
+      type: Function,
+      required: true,
+    },
+    onCloseDelete: {
+      type: Function,
       required: true,
     },
     sortBy: {
@@ -203,48 +298,25 @@ export default {
           ]
         },
   
-        editItem (item) {
-          this.editedIndex = this.desserts.indexOf(item)
-          this.editedItem = Object.assign({}, item)
-          this.dialog = true
-        },
-  
-        deleteItem (item) {
-          this.editedIndex = this.desserts.indexOf(item)
-          this.editedItem = Object.assign({}, item)
-          this.dialogDelete = true
-        },
-  
-        deleteItemConfirm () {
-          this.desserts.splice(this.editedIndex, 1)
-          this.closeDelete()
-        },
-  
-        close () {
-          this.dialog = false
-          this.$nextTick(() => {
-            this.editedItem = Object.assign({}, this.defaultItem)
-            this.editedIndex = -1
-          })
-        },
-  
-        closeDelete () {
-          this.dialogDelete = false
-          this.$nextTick(() => {
-            this.editedItem = Object.assign({}, this.defaultItem)
-            this.editedIndex = -1
-          })
-        },
-  
-        save () {
-          if (this.editedIndex > -1) {
-            Object.assign(this.desserts[this.editedIndex], this.editedItem)
-          } else {
-            this.desserts.push(this.editedItem)
-          }
-          this.close()
-        },
+        editItem(item) {
+    this.$emit('edit-item', item);
+  },
+  deleteItem(item) {
+    this.$emit('delete-item', item);
+  },
+  deleteItemConfirm() {
+    this.$emit('delete-item-confirm');
+  },
+  close() {
+    this.$emit('close');
+  },
+  closeDelete() {
+    this.$emit('close-delete');
+  },
+  save() {
+    this.$emit('save', this.editedItem);
+  },
       },
     },
   }
-  </script>
+  </script> -->
--- frontend/src/views/VisitRecord.vue
+++ frontend/src/views/VisitRecord.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
     <data-table
+      :edited-item="editedItem"
+      @update:edited-item="updateEditedItem"
+      @save="saveItem"
       :headers="tableHeaders"
       :items="tableItems"
+      :default-item="defaultItem"
+      @edit-item="editItem"
+      @delete-item="deleteItem"
+      @delete-item-confirm="deleteItemConfirm"
+      @close="close"
+      @close-delete="closeDelete"
       sort-by="calories"
       sort-order="asc"
     ></data-table>
@@ -36,7 +45,59 @@ export default {
         },
         // Add more items as needed
       ],
+      editedIndex: -1,
+      editedItem: {
+        name: '',
+        calories: 0,
+        fat: 0,
+        carbs: 0,
+        protein: 0,
+      },
+      defaultItem: {
+        name: '',
+        calories: 0,
+        fat: 0,
+        carbs: 0,
+        protein: 0,
+      },
     };
+  },
+  methods: {
+    editItem(item) {
+      this.editedIndex = this.tableItems.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+    },
+    deleteItem(item) {
+      this.editedIndex = this.tableItems.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+    },
+    deleteItemConfirm() {
+      // Perform the delete operation in the database
+      // Remove the item from the tableItems array
+      this.tableItems.splice(this.editedIndex, 1);
+      this.closeDelete();
+    },
+    close() {
+      this.editedIndex = -1;
+      this.editedItem = Object.assign({}, this.defaultItem);
+    },
+    closeDelete() {
+      this.editedIndex = -1;
+      this.editedItem = Object.assign({}, this.defaultItem);
+    },
+    updateEditedItem(item) {
+      this.editedItem = item;
+    },
+    saveItem(item) {
+      if (this.editedIndex > -1) {
+        // Perform the update operation in the database
+        Object.assign(this.tableItems[this.editedIndex], item);
+      } else {
+        // Perform the create operation in the database
+        this.tableItems.push(item);
+      }
+      this.close();
+    },
   },
 };
 </script>
