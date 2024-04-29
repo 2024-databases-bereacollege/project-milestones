7a1ef4a82a56bb391051ea9f3abc084390928f86
hamiltonnBC <hamiltonn@berea.edu>
2024-04-28 22:35:42 -0400

    added update to models, adjusted visit service and record.

--- flaskapp/models.py
+++ flaskapp/models.py
@@ -56,7 +56,7 @@ class Visit_Record(baseModel):
     ServiceOrder = AutoField() #IntegerField(primary_key=True)
     NeighborID = ForeignKeyField(Neighbor, backref='visit_record')  # Ensured consistency in backref
     VolunteerID = ForeignKeyField(Volunteer, backref='visit_record')  # Ensured consistency in backref
-    RecordID = ForeignKeyField(Visit_Record, backref='visit_record')
+    RecordID = ForeignKeyField(Visit_Service, backref='visit_record') # Changed to Visit Service
 
 
 class Inventory_Usage(baseModel):
